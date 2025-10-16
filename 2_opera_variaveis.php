f47eba68775ebcaf9519e1eb64d1514231f5d2f4
Gabrielly  de Paula <gabrielly.r.paula@aluno.senai.br>
2025-10-16 09:26:30 -0300

    Inicio

---
+++ 2_opera_variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+//Montagem de url
+// https://localhost/php_basico_aula/php-basicos(alunos)/
+// 2_opera_variaveis.php?numero1=10&numero2=5
+
+//  Variáveis que recebem valores pela URL (Metodo GET)
+$numero1 = $_GET[ 'numero1'];
+$numero1 = $_GET[ 'numero2'];
+
+
+?>
---
+++ Aula1/exemplo3.php
@@ -0,0 +1,9 @@
+<?php
+echo                              "testado espaço em branco <br>";
+echo "testando espaço em branco <br>";
+
+// O caracter   adicioma um espaço branco dentro da string
+echo "&#160; &#160                                        testando       espaço            em           branco <br>";
+echo "quebra de linha <br>"
+;
+?>
